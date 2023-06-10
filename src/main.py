4ffdd398af1b2fc44d8516c1364fbea107b2f22b
Dmitriy Trofimov <a.dev.mobile@gmail.com>
2023-06-10 19:15:10 +0300

    feat:

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
+
 app = FastAPI()
 
 
 @app.get("/")
-def hello():
-    return {"message": "Hello, FastAPI!"}
+def read_root():
+    return {"Hello": "World112"}
 
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    print(item_id)
+    return {"item_id": item_id}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
